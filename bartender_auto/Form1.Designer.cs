d31345f4752295fee42ff1b29e52796209aad6df
colin <songyunlin-311311@163.com>
2023-05-22 14:57:21 +0800

    区分dll的64位运行程序生成

--- bartender_auto/Form1.Designer.cs
+++ bartender_auto/Form1.Designer.cs
@@ -56,28 +56,25 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(15, 44);
-			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Location = new System.Drawing.Point(10, 29);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(94, 21);
+			this.label1.Size = new System.Drawing.Size(63, 14);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "模板文件";
 			// 
 			// txtTemplateFile
 			// 
-			this.txtTemplateFile.Location = new System.Drawing.Point(118, 39);
-			this.txtTemplateFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtTemplateFile.Location = new System.Drawing.Point(79, 26);
 			this.txtTemplateFile.Name = "txtTemplateFile";
 			this.txtTemplateFile.ReadOnly = true;
-			this.txtTemplateFile.Size = new System.Drawing.Size(358, 31);
+			this.txtTemplateFile.Size = new System.Drawing.Size(240, 23);
 			this.txtTemplateFile.TabIndex = 1;
 			// 
 			// btnFileOpen
 			// 
-			this.btnFileOpen.Location = new System.Drawing.Point(488, 39);
-			this.btnFileOpen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnFileOpen.Location = new System.Drawing.Point(325, 26);
 			this.btnFileOpen.Name = "btnFileOpen";
-			this.btnFileOpen.Size = new System.Drawing.Size(86, 34);
+			this.btnFileOpen.Size = new System.Drawing.Size(57, 23);
 			this.btnFileOpen.TabIndex = 2;
 			this.btnFileOpen.Text = "浏览...";
 			this.btnFileOpen.UseVisualStyleBackColor = true;
@@ -86,10 +83,9 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(93, 171);
-			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Location = new System.Drawing.Point(62, 114);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(62, 18);
+			this.label2.Size = new System.Drawing.Size(41, 12);
 			this.label2.TabIndex = 3;
 			this.label2.Text = "打印机";
 			// 
@@ -97,33 +93,30 @@
 			// 
 			this.listPrinter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.listPrinter.FormattingEnabled = true;
-			this.listPrinter.Location = new System.Drawing.Point(171, 166);
-			this.listPrinter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.listPrinter.Location = new System.Drawing.Point(114, 111);
 			this.listPrinter.Name = "listPrinter";
-			this.listPrinter.Size = new System.Drawing.Size(358, 26);
+			this.listPrinter.Size = new System.Drawing.Size(240, 20);
 			this.listPrinter.TabIndex = 4;
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(75, 220);
-			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Location = new System.Drawing.Point(50, 147);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(80, 18);
+			this.label3.Size = new System.Drawing.Size(53, 12);
 			this.label3.TabIndex = 5;
 			this.label3.Text = "条码数量";
 			// 
 			// numCodesQuantity
 			// 
-			this.numCodesQuantity.Location = new System.Drawing.Point(171, 213);
-			this.numCodesQuantity.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.numCodesQuantity.Location = new System.Drawing.Point(114, 142);
 			this.numCodesQuantity.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
 			this.numCodesQuantity.Name = "numCodesQuantity";
-			this.numCodesQuantity.Size = new System.Drawing.Size(102, 28);
+			this.numCodesQuantity.Size = new System.Drawing.Size(68, 21);
 			this.numCodesQuantity.TabIndex = 9;
 			this.numCodesQuantity.Value = new decimal(new int[] {
             1,
@@ -137,11 +130,9 @@
 			this.groupBox1.Controls.Add(this.label1);
 			this.groupBox1.Controls.Add(this.btnFileOpen);
 			this.groupBox1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.groupBox1.Location = new System.Drawing.Point(18, 18);
-			this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.groupBox1.Size = new System.Drawing.Size(582, 96);
+			this.groupBox1.Size = new System.Drawing.Size(388, 64);
 			this.groupBox1.TabIndex = 10;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "打印模板";
@@ -150,10 +141,9 @@
 			// 
 			this.btnPrint.Enabled = false;
 			this.btnPrint.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.btnPrint.Location = new System.Drawing.Point(38, 522);
-			this.btnPrint.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnPrint.Location = new System.Drawing.Point(25, 348);
 			this.btnPrint.Name = "btnPrint";
-			this.btnPrint.Size = new System.Drawing.Size(548, 52);
+			this.btnPrint.Size = new System.Drawing.Size(365, 35);
 			this.btnPrint.TabIndex = 13;
 			this.btnPrint.Text = "打印";
 			this.btnPrint.UseVisualStyleBackColor = true;
@@ -167,43 +157,39 @@
 			// 
 			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(171, 309);
-			this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.comboBox1.Location = new System.Drawing.Point(114, 206);
 			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(358, 26);
+			this.comboBox1.Size = new System.Drawing.Size(240, 20);
 			this.comboBox1.TabIndex = 4;
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(93, 314);
-			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Location = new System.Drawing.Point(62, 209);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(62, 18);
+			this.label4.Size = new System.Drawing.Size(41, 12);
 			this.label4.TabIndex = 3;
 			this.label4.Text = "打印机";
 			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(75, 363);
-			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label5.Location = new System.Drawing.Point(50, 242);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(80, 18);
+			this.label5.Size = new System.Drawing.Size(53, 12);
 			this.label5.TabIndex = 5;
 			this.label5.Text = "条码数量";
 			// 
 			// numericUpDown1
 			// 
-			this.numericUpDown1.Location = new System.Drawing.Point(171, 356);
-			this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.numericUpDown1.Location = new System.Drawing.Point(114, 237);
 			this.numericUpDown1.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
 			this.numericUpDown1.Name = "numericUpDown1";
-			this.numericUpDown1.Size = new System.Drawing.Size(102, 28);
+			this.numericUpDown1.Size = new System.Drawing.Size(68, 21);
 			this.numericUpDown1.TabIndex = 9;
 			this.numericUpDown1.Value = new decimal(new int[] {
             1,
@@ -214,11 +200,9 @@
 			// groupBox2
 			// 
 			this.groupBox2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.groupBox2.Location = new System.Drawing.Point(18, 136);
-			this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.groupBox2.Location = new System.Drawing.Point(12, 91);
 			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.groupBox2.Size = new System.Drawing.Size(582, 126);
+			this.groupBox2.Size = new System.Drawing.Size(388, 84);
 			this.groupBox2.TabIndex = 14;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "打印机1";
@@ -226,11 +210,9 @@
 			// groupBox3
 			// 
 			this.groupBox3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.groupBox3.Location = new System.Drawing.Point(16, 273);
-			this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.groupBox3.Location = new System.Drawing.Point(11, 182);
 			this.groupBox3.Name = "groupBox3";
-			this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.groupBox3.Size = new System.Drawing.Size(582, 126);
+			this.groupBox3.Size = new System.Drawing.Size(388, 84);
 			this.groupBox3.TabIndex = 15;
 			this.groupBox3.TabStop = false;
 			this.groupBox3.Text = "打印机2";
@@ -238,20 +220,18 @@
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(63, 447);
-			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label6.Location = new System.Drawing.Point(42, 298);
 			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(89, 18);
+			this.label6.Size = new System.Drawing.Size(59, 12);
 			this.label6.TabIndex = 16;
 			this.label6.Text = "扫描/输入";
 			// 
 			// textBox1
 			// 
 			this.textBox1.Enabled = false;
-			this.textBox1.Location = new System.Drawing.Point(171, 442);
-			this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textBox1.Location = new System.Drawing.Point(114, 295);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(224, 28);
+			this.textBox1.Size = new System.Drawing.Size(151, 21);
 			this.textBox1.TabIndex = 17;
 			// 
 			// checkAutoPrint
@@ -259,10 +239,9 @@
 			this.checkAutoPrint.AutoSize = true;
 			this.checkAutoPrint.Checked = true;
 			this.checkAutoPrint.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.checkAutoPrint.Location = new System.Drawing.Point(423, 447);
-			this.checkAutoPrint.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.checkAutoPrint.Location = new System.Drawing.Point(282, 298);
 			this.checkAutoPrint.Name = "checkAutoPrint";
-			this.checkAutoPrint.Size = new System.Drawing.Size(106, 22);
+			this.checkAutoPrint.Size = new System.Drawing.Size(72, 16);
 			this.checkAutoPrint.TabIndex = 18;
 			this.checkAutoPrint.Text = "自动打印";
 			this.checkAutoPrint.UseVisualStyleBackColor = true;
@@ -271,20 +250,18 @@
 			// groupBox4
 			// 
 			this.groupBox4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.groupBox4.Location = new System.Drawing.Point(18, 408);
-			this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.groupBox4.Location = new System.Drawing.Point(12, 272);
 			this.groupBox4.Name = "groupBox4";
-			this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.groupBox4.Size = new System.Drawing.Size(582, 90);
+			this.groupBox4.Size = new System.Drawing.Size(388, 60);
 			this.groupBox4.TabIndex = 16;
 			this.groupBox4.TabStop = false;
 			this.groupBox4.Text = "SN号录入";
 			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(622, 588);
+			this.ClientSize = new System.Drawing.Size(415, 392);
 			this.Controls.Add(this.checkAutoPrint);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.label6);
@@ -303,9 +280,8 @@
 			this.Controls.Add(this.groupBox4);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.KeyPreview = true;
-			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Name = "Form1";
-			this.Text = "Bartender标签打印 V201905";
+			this.Text = "Bartender标签打印 V201905 X64";
 			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
